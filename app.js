eb91ef5f58797b7956afcb91bb542d4820feaf88
Juan Agustin Alejandro <juan_agustin_alejandro@hotmail.com>
2021-11-26 13:03:55 -0300

    Funciones Tarea
    Se realizo los ejercicios de funciones

--- app.js
+++ app.js
@@ -195,3 +195,46 @@ const tarea6 = function () {
 
     tareas.splice(tareas.length - 1, 1, remplazar)
 }
+
+/*
+Funciones
+-------------
+
+- Crear una función llamada agregarProducto que pida al usuario ingresar un producto y lo guarde en un arreglo.
+ - Se puede hacer que la función pida ingresar productos una y otra vez hasta que se presione cancelar en el prompt 
+ o se deje vacio el campo y se presione aceptar.
+ - Al final la función debe mostrar por consola la lista de los productos ingresados.
+
+- Crear una función anónima que reciba como parámetros nombre, apellido, domicilio y correo electrónico.
+ - La función debe recibir los parámetros y mostrar en el navegador el siguiente mensaje: 'Me llamo <nombre> <apellido> vivo en <domicilio>, si deseas contactarme te dejo mi correo electrónico <email>. Que tengas un buen día.
+ - Para mostrar texto en el navegador debemos usar document.write()
+ 
+*/
+/*
+let prueba = prompt(`Ingresar producto (para salir dejar vacio o cancelar):`)
+console.log(!prueba)*/
+
+let agregarProducto = function () {
+    
+    let arreglo = [];
+    let i = -1;
+    let agregar;
+    do {
+        agregar = prompt(`Ingresar producto (para salir dejar vacio o cancelar):`);
+        if (agregar) {
+            arreglo.push(agregar)
+            i++;    
+        }
+        
+    } while (agregar)
+    i=0
+    arreglo.forEach(element => {
+        console.log(`${i++} - ${element}`)
+    });
+    return arreglo;
+}
+
+let datosPersonales = function (nombre, apellido, domicilio, correo) {
+    document.write(`Me llamo ${nombre} ${apellido} vivo en ${domicilio}, si deseas 
+    contactarme te dejo mi correo electrónico ${correo}. Que tengas un buen día.`)
+}
